8eb039fe417ee074bf950ea029fba6cada8be32e
chy <kombo6768@gmail.com>
2018-07-18 01:12:04 +0900

    Project With Gg

---
+++ config/header.php
@@ -0,0 +1,8 @@
+<div class="header_section">
+  <div class="header_section_list"><a href="#">Macfa</a></div>
+  <div class="header_section_list"><a href="#">Web</a></div>
+  <div class="header_section_list"><a href="#">Application</a></div>
+  <div class="header_section_list"><a href="#">Database</a></div>
+  <div class="header_section_list"><a href="#">Linux</a></div>
+  <div class="header_section_list"><a href="#">Tool</a></div>
+</div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Macfa</title>
+	<?php include_once 'config/config.php'; ?>
+</head>
+<body>
+<?php include_once 'config/header.php'; ?>
+<div class="container">
+	<div class="main">
+		
+	</div>
+</div>
+</body>
+</html>
