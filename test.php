1636b0ae5f6cb17c2acd976c731137939e3cea67
chy <kombo6768@gmail.com>
2018-08-10 20:30:14 +0900

    Test for merge

--- test.php
+++ test.php
@@ -17,14 +17,7 @@
 			<div class="main_section_item">
 				<h2>환영합니다.</h2>
 				<p>
-					이것저것 해보는 사이트입니다. sajdioasjdioajsTest FIle
-
-					<ul>
-						<li>html, css, js ( frontend )</li>
-						<li>jquery, vuejs ( hot Frontend FrameWork )</li>
-						<li>php, java ( backend )</li>
-						<li>lalabel ( hot Backend FrameWork )</li>
-						<li>Linux ( server )</li>
+					  . sajdioasjdioajsTest FIle
 					</ul>
 				</p>
 			</div>
